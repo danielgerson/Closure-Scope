e000c31405299073770b19cce13a166cfe0d2176
Daniel Barrios <gersonbarrios97@gmail.com>
2022-10-16 17:49:30 -0600

    add class 4 reasignacion/redeclaracion

---
+++ src/scope/re.js
@@ -0,0 +1,28 @@
+// Reasignación, Redeclaración
+
+var firstName; // Declarando, se le asigna undefined
+firstName = 'Daniel';
+console.log(firstName);
+
+var lastName = 'Barrios' // Declarando y asignando valor
+lastName = 'Navarro'; // Reasignando nuevo valor a lastName
+console.log(lastName);
+
+var secondName = 'Santi'; // Declarando y asignando valor
+var secondName = 'Lucas'; // Reasignando valor a secondName
+console.log(secondName)
+
+// Let
+let fruit = 'Apple'; // Declarando y asignando valor
+fruit = 'Kiwi'; // Reasignando valor
+console.log(fruit);
+// let fruit = 'Banana';
+
+// const 
+const animal = 'Dog'; // Declara y reasignando
+animal = 'Cat';
+console.log(animal);
+
+const vehicles = [];
+vehicles.push('BMW');
+console.log(vehicles);

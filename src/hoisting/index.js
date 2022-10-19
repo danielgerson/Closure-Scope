aac30fcf36ff489c12e3c0735d489017af73f3b7
Daniel Barrios <gersonbarrios97@gmail.com>
2022-10-19 11:46:48 -0600

    add class 8 hoisting

---
+++ src/hoisting/index.js
@@ -0,0 +1,13 @@
+/* var nameOfDog;
+
+console.log(nameOfDog);
+var nameOfDog = 'Terry';
+console.log(nameOfDog); */
+
+nameOfDog();
+
+function nameOfDog () {
+  console.log(`El mejor perrito es ${terry}`);
+}
+
+var terry = 'Terry';
